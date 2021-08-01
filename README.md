a9020d952af0b782cf414bd23c8642d6d747001b
DanielPipaAguirre <daniel.pipa@trade.ec>
2021-08-01 17:37:15 -0500

    passport

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # CoderServidorExpress
 Servidor en Express del curso Backend de CoderHouse 🚀
 
-Login/Logout
+## Passport
 
 - La ruta principal es http://localhost:8080/login
-- Luego ir a http://localhost:8080/contenido para verficar el login
--- index.js
+++ index.js
@@ -1,156 +1,190 @@
 const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
+const exphbs = require("express-handlebars");
+
+const passport = require("passport");
+const bcrypt = require("bcrypt");
+const LocalStrategy = require("passport-local").Strategy;
+const routes = require("./routes");
 const config = require("./config/config.json");
-const session = require("express-session");
-const handlebars = require("express-handlebars");
+const controllersdb = require("./api/users");
+const User = require("./models/users");
 
-const chat = require("./api/chat");
 
-const { Server } = require("socket.io");
-const io = new Server(server);
-
-const normalizr = require("normalizr");
-const normalize = normalizr.normalize;
-const schema = normalizr.schema;
-
-require("./database/connection");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.engine(
-    "hbs",
-    handlebars({
-        extname: ".hbs",
-        defaultLayout: "index.hbs",
-        layoutsDir: __dirname + "/views",
-        partialsDir: __dirname + "/views",
-    })
+passport.use(
+    "login",
+    new LocalStrategy(
+        {
+            passReqToCallback: true,
+        },
+        function (req, username, password, done) {
+            User.findOne({ username: username }, function (err, user) {
+                if (err) return done(err);
+                if (!user) {
+                    console.log("User Not Found with username " + username);
+                    return done(
+                        null,
+                        false,
+                        console.log("message", "User Not found.")
+                    );
+                }
+                if (!isValidPassword(user, password)) {
+                    console.log("Invalid Password");
+                    return done(
+                        null,
+                        false,
+                        console.log("message", "Invalid Password")
+                    );
+                }
+                return done(null, user);
+            });
+        }
+    )
 );
 
-app.set("view engine", "hbs");
-
-app.set("views", "./views");
-
-app.use(
-    session({
-        secret: "secreto",
-        resave: true,
-        saveUninitialized: true,
-        cookie: { maxAge: 100000 },
-    })
-);
-
-app.get("/login", (req, res) => {
-    res.sendFile(__dirname + "/public/login.html");
-});
-
-app.post("/login", (req, res) => {
-    const { username } = req.body;
-    if (username) {
-        req.session.username = "daniel";
-        return res.json({ username: username });
-    }
-    return res.json({ error: "No se pude logear" });
-});
-
-const auth = (req, res, next) => {
-    if(!req.session.username) {
-        return res.redirect('/login')
-    }
-    if (req.session && req.session.username == "daniel") {
-        return next();
-    } else {
-        return res.status(401).send("No autorizado");
-    }
+var isValidPassword = function (user, password) {
+    return bcrypt.compareSync(password, user.password);
 };
 
-app.get("/logout", auth, (req, res) => {
-    const username = req.session.username;
-    req.session.destroy((err) => {
-        if (!err) res.render("vista", { username: username });
-        else res.send({ status: "Logout ERROR", body: err });
+passport.use(
+    "signup",
+    new LocalStrategy(
+        {
+            passReqToCallback: true,
+        },
+        function (req, username, password, done) {
+            findOrCreateUser = function () {
+                User.findOne({ username: username }, function (err, user) {
+                    if (err) {
+                        console.log("Error in SignUp: " + err);
+                        return done(err);
+                    }
+                    if (user) {
+                        console.log("User already exists");
+                        return done(
+                            null,
+                            false,
+                            console.log("message", "User Already Exists")
+                        );
+                    } else {
+                        const newUser = new User();
+                        newUser.username = username;
+                        newUser.password = createHash(password);
+    
+                        newUser.save(function (err) {
+                            if (err) {
+                                console.log("Error in Saving user: " + err);
+                                throw err;
+                            }
+                            console.log("User Registration succesful");
+                            return done(null, newUser);
+                        });
+                    }
+                });
+            };
+
+            process.nextTick(findOrCreateUser);
+        }
+    )
+);
+var createHash = function (password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+passport.serializeUser(function (user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
     });
 });
 
-app.get("/contenido", auth, (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+// ------------------------------------------------------------------------------
+//  EXPRESS
+// ------------------------------------------------------------------------------
+
+const app = express();
+app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "main.hbs" }));
+app.set("view engine", ".hbs");
+
+app.use(express.static(__dirname + "/views"));
+app.use(require("cookie-parser")());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+    require("express-session")({
+        secret: "keyboard cat",
+        cookie: {
+            httpOnly: false,
+            secure: false,
+            maxAge: 10 * 60,
+        },
+        rolling: true,
+        resave: true,
+        saveUninitialized: false,
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// ------------------------------------------------------------------------------
+//  ROUTING GET POST
+// ------------------------------------------------------------------------------
+//  INDEX
+app.get("/", routes.getRoot);
+
+//  LOGIN
+app.get("/login", routes.getLogin);
+app.post(
+    "/login",
+    passport.authenticate("login", { failureRedirect: "/faillogin" }),
+    routes.postLogin
+);
+app.get("/faillogin", routes.getFaillogin);
+
+//  SIGNUP
+app.get("/signup", routes.getSignup);
+app.post(
+    "/signup",
+    passport.authenticate("signup", { failureRedirect: "/failsignup" }),
+    routes.postSignup
+);
+app.get("/failsignup", routes.getFailsignup);
+
+function checkAuthentication(req, res, next) {
+    if (req.isAuthenticated()) {
+        //req.isAuthenticated() will return true if user is logged in
+        next();
+    } else {
+        res.redirect("/login");
+    }
+}
+
+app.get("/ruta-protegida", checkAuthentication, (req, res) => {
+    //do something only if user is authenticated
+    var user = req.user;
+    console.log(user);
+    res.send("<h1>Ruta OK!</h1>");
 });
 
-app.get("/loginData", auth, (req, res) => {
-    res.json({ username: req.session.username });
-});
+//  LOGOUT
+app.get("/logout", routes.getLogout);
 
-io.on("connection", async (socket) => {
-    console.log("Nuevo cliente conectado");
+//  FAIL ROUTE
+app.get("*", routes.failRoute);
 
-    const messages = await chat.findAll();
+// ------------------------------------------------------------------------------
+//  LISTEN SERVER
+// ------------------------------------------------------------------------------
+controllersdb.conectarDB(config.MONGO_URL, (err) => {
+    if (err) return console.log("error en conexión de base de datos", err);
+    console.log("BASE DE DATOS CONECTADA");
 
-    // json normalize
-    const mensajesConId = {
-        id: "mensajes",
-        mensajes: messages.map((mensaje) => ({ ...mensaje._doc })),
-    };
-    const schemaAuthor = new schema.Entity("author", {}, { idAttribute: "id" });
-
-    const schemaMensaje = new schema.Entity(
-        "post",
-        { author: schemaAuthor },
-        { idAttribute: "_id" }
-    );
-    const schemaMensajes = new schema.Entity(
-        "posts",
-        { mensajes: [schemaMensaje] },
-        { idAttribute: "id" }
-    );
-
-    const normalizedData = normalize(mensajesConId, schemaMensajes);
-
-    io.sockets.emit("mensajes", normalizedData);
-
-    socket.on("nuevo-mensaje", async function (data) {
-        await chat.create(data);
-        const messages = await chat.findAll();
-
-        // json normalize
-        const mensajesConId = {
-            id: "mensajes",
-            mensajes: messages.map((mensaje) => ({ ...mensaje._doc })),
-        };
-        const schemaAuthor = new schema.Entity(
-            "author",
-            {},
-            { idAttribute: "id" }
-        );
-
-        const schemaMensaje = new schema.Entity(
-            "post",
-            { author: schemaAuthor },
-            { idAttribute: "_id" }
-        );
-        const schemaMensajes = new schema.Entity(
-            "posts",
-            { mensajes: [schemaMensaje] },
-            { idAttribute: "id" }
-        );
-
-        const normalizedData = normalize(mensajesConId, schemaMensajes);
-
-        io.sockets.emit("mensajes", normalizedData);
+    app.listen(config.PORT, function (err) {
+        if (err) return console.log("error en listen server", err);
+        console.log(`Server running on port ${config.PORT}`);
     });
-});
-
-const routerProduct = require("./routes/productos");
-app.use("/api", routerProduct);
-
-const puerto = process.env.PORT || config.PORT;
-
-const serverExpress = server.listen(puerto, () => {
-    console.log(`servidor escuchando en http://localhost:${puerto}`);
-});
-
-serverExpress.on("error", (error) => {
-    console.log("error en el servidor:", error);
 });
